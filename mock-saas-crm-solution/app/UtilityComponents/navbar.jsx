536b3160b870efce309eeafb4f232b1e216290cc
Thomas Marion <tmarion19@outlook.com>
2025-12-01 13:18:06 +1000

    setup basic page routes

--- mock-saas-crm-solution/app/UtilityComponents/navbar.jsx
+++ mock-saas-crm-solution/app/UtilityComponents/navbar.jsx
@@ -1,13 +1,18 @@
+import { useEffect, useState } from 'react';
 import './navbar.css'
 
-export function navbar()
+export default function Navbar()
 {
-    
     return (
-        <div className='navbarContainer'>
+        <nav className='navbarContainer'>
+                <div className='navbarLeft'>
+                    <h1>SaaS CRM App</h1>
+                </div>
 
-
-            
-        </div>
+                <div className='navbarRight'>
+                    <a className='homeButton' href='/home'>Home</a>
+                    <a className='loginButton' href='/login'>Login</a>
+                </div>
+        </nav>
     );
 }
---
+++ mock-saas-crm-solution/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './page.css';
+
+export default function Home()
+{
+    return(
+        <main>
+            HomePage
+        </main>
+    );
+}   
