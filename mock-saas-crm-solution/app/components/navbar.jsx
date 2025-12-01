115e3b0a5dabf2fb3fec7f6c9103d9d34b9d4f0b
Thomas Marion <tmarion19@outlook.com>
2025-12-01 17:24:05 +1000

    directory restructure

--- mock-saas-crm-solution/app/components/navbar.jsx
+++ mock-saas-crm-solution/app/components/navbar.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import './navbar.css'
 
 export default function Navbar()
--- mock-saas-crm-solution/next.config.ts
+++ mock-saas-crm-solution/next.config.ts
@@ -4,4 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
