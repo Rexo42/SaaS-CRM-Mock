92b0a051f69aa524891095a355764cbe6e6165c4
Thomas Marion <tmarion19@outlook.com>
2025-12-01 17:49:43 +1000

    restructure

--- mock-saas-crm-solution/app/layout.tsx
+++ mock-saas-crm-solution/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // component import
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/navbar/navbar.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
